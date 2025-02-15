406172e4f972adaa303cf779389472f26deb0332
Hariz Hakim <harizhakim85@gmail.com>
2025-02-16 03:04:15 +0800

    added font and add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
+int counter = 0;
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('My First app'),
-      centerTitle: true,
-    ),
-    body: const Center(
-      child: Text('Hello, world!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        print('You pressed the button');
-      },
-      child: const Text('Click me'),
-    ),
-  ),
+void main() => runApp(const MaterialApp(
+  home: HomePage(),
 ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+
+void incrementCounter() {
+  setState(() {
+    counter++;
+  });
+}
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First App',),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreenAccent,
+      ),
+        body: Center(
+        child: Column(
+          children: [
+            const Image(
+              image: AssetImage('./assets/goat.jpg'),
+              width: 300, 
+              height: 300,
+            ),
+            Text('This is a goat $counter'), 
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementCounter,
+        child: const Text(
+          'Click me',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      ),
+    );
+  }
+}
 
